722a8b4a03b02051593e5741047f3ab9e731ba2f
legal-s <legal-s@mgdis.fr>
2024-03-19 09:27:25 +0100

    chore: add bug 04

--- src/test/java/fr/mgdis/aspose/words/AsposeWordsBugTest.java
+++ src/test/java/fr/mgdis/aspose/words/AsposeWordsBugTest.java
@@ -52,26 +52,8 @@ class AsposeWordsBugTest {
           PDFTextStripper stripper = new PDFTextStripper();
           String text = stripper.getText(pdfDocument);
 
-          // Check "Domiciliations bancaires"
-          assertTrue(text.contains("Relevé d’identité bancaire : "));
-          assertTrue(text.contains("word-template.docx - 07/12/2023 (19,34Ko)"));
-          assertTrue(text.contains("Note : Mon template word. On peut avoir que un seul document"));
-
-          // Check "Pièces"
-          assertTrue(text.contains("Pièces fournies :"));
-          assertTrue(text.contains("Pièce d'identité :"));
-          assertTrue(text.contains("data-sources.pdf - 07/12/2023 (145,65Ko)"));
-          assertTrue(text.contains("Note : Data Sources"));
-
-          assertTrue(text.contains("Cahier des charges rédigé par le demandeur : Transmis par envoi postal"));
-
-          assertTrue(text.contains("Propositions et devis des bureaux d'étude consultés :"));
-          assertTrue(text.contains("word-template.docx - 07/12/2023 (19,34Ko)"));
-          assertTrue(text.contains("Note : Mon template word"));
-
-          assertTrue(text.contains("Pièce conditionnée au Plan de Financement TTC :"));
-          assertTrue(text.contains("Toute pièce utile à la présentation de votre projet :"));
-          assertTrue(text.contains("depot_TELE_TEST.docx - 07/12/2023 (50,94Ko)"));
+          // Check that the document is correctly generated
+          assertNotNull(text);
         }
       }
     }
@@ -124,6 +106,48 @@ class AsposeWordsBugTest {
       try (InputStream template = new FileInputStream("src/test/resources/templates/template_03.docx")) {
         var document = asposeWordsBuilder.buildDocument(template, new DataSourceRoot(transformedDatasets));
         assertNotNull(document);
+      }
+    }
+  }
+
+  @Nested
+  class Bug04 {
+    @Test
+    @DisplayName("should must generate a word document with the data as with an old version of Aspose")
+    void testWordMergeOperationWithAnOldWordTemplate() throws Exception {
+      try (InputStream template = new FileInputStream("src/test/resources/templates/template_04.docx")) {
+
+        String content = new String(Files.readAllBytes(Paths.get("src/test/resources/data/data_04.json")));
+
+        // IMailMergeDataSourceRoot that contains our data for the merge operation
+        var dataSourceRoot = new JSONMailMergeDataSourceRoot(content);
+        var document = asposeWordsBuilder.buildDocument(template, dataSourceRoot);
+
+        try (PDDocument pdfDocument = PDDocument.load(document.toByteArray())) {
+          PDFTextStripper stripper = new PDFTextStripper();
+          String text = stripper.getText(pdfDocument);
+
+          // Check "Domiciliations bancaires"
+          assertTrue(text.contains("Relevé d’identité bancaire : "));
+          assertTrue(text.contains("word-template.docx - 07/12/2023 (19,34Ko)"));
+          assertTrue(text.contains("Note : Mon template word. On peut avoir que un seul document"));
+
+          // Check "Pièces"
+          assertTrue(text.contains("Pièces fournies :"));
+          assertTrue(text.contains("Pièce d'identité :"));
+          assertTrue(text.contains("data-sources.pdf - 07/12/2023 (145,65Ko)"));
+          assertTrue(text.contains("Note : Data Sources"));
+
+          assertTrue(text.contains("Cahier des charges rédigé par le demandeur : Transmis par envoi postal"));
+
+          assertTrue(text.contains("Propositions et devis des bureaux d'étude consultés :"));
+          assertTrue(text.contains("word-template.docx - 07/12/2023 (19,34Ko)"));
+          assertTrue(text.contains("Note : Mon template word"));
+
+          assertTrue(text.contains("Pièce conditionnée au Plan de Financement TTC :"));
+          assertTrue(text.contains("Toute pièce utile à la présentation de votre projet :"));
+          assertTrue(text.contains("depot_TELE_TEST.docx - 07/12/2023 (50,94Ko)"));
+        }
       }
     }
   }
